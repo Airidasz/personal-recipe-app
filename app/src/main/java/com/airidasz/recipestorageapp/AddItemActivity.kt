c592195fccaaffd361cf639df0941538c6282ee7
Airidasz <61290246+Airidasz@users.noreply.github.com>
2020-10-21 13:04:25 +0300

    NestedScrollView scrolls to bottom to fit show last ingredient

--- app/src/main/java/com/airidasz/recipestorageapp/AddItemActivity.kt
+++ app/src/main/java/com/airidasz/recipestorageapp/AddItemActivity.kt
@@ -4,19 +4,13 @@ import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.graphics.Bitmap
-import android.graphics.Rect
 import android.os.Bundle
 import android.provider.MediaStore
-import android.util.DisplayMetrics
 import android.view.LayoutInflater
 import android.view.View
-import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.coordinatorlayout.widget.CoordinatorLayout
-import com.google.android.material.appbar.AppBarLayout
 import kotlinx.android.synthetic.main.activity_add_item.*
-import kotlinx.android.synthetic.main.activity_search.*
 import kotlinx.android.synthetic.main.ingredient_layout.view.*
 
 
@@ -73,17 +67,7 @@ class AddItemActivity() : AppCompatActivity() {
 
             ingredient_list.addView(myView)
 
-//            val params = AppBarLayout.LayoutParams(
-//                AppBarLayout.LayoutParams.MATCH_PARENT,
-//                AppBarLayout.LayoutParams.MATCH_PARENT
-//            )
-////            val a = add_recipe_fields.height
-////
-////            Toast.makeText(this, "$a", Toast.LENGTH_SHORT).show()
-//
-//
-//            add_item_scroll.layoutParams = params
-
+            add_item_nested_scroll.post(Runnable { add_item_nested_scroll.fullScroll(View.FOCUS_DOWN) })
         }
     }
 
